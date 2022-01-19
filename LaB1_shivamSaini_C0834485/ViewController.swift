4a5065a8c2ef7ae11e798dbcc0b3f8beaf199006
shivamsaini485 <97565572+shivamsaini485@users.noreply.github.com>
2022-01-18 21:25:31 -0500

    final commit

--- LaB1_shivamSaini_C0834485/ViewController.swift
+++ LaB1_shivamSaini_C0834485/ViewController.swift
@@ -9,11 +9,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttons: [UIButton]!
+    var board = [String]()
+    var activePlayer = ""
+    
+    var rules = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeGesture))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        loadBoard()
+        print(board)
+        
     }
-
+    @objc func swipeGesture(){
+        reset()
+    }
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        let index = buttons.index(of: sender)!
+        
+        
+        if !board[index].isEmpty {
+            return
+        }
+        
+        
+        if activePlayer == "X"{
+            sender.setTitle("O", for: .normal)
+            activePlayer = "O"
+            //to freeze it so not able to make move again
+            board[index] = "X"
+        }else{
+            sender.setTitle("X", for: .normal)
+            activePlayer = "X"
+            board[index] = "O"
+        }
+        winner()
+    }
+    func winner(){
+        for rule in rules {
+            let player1 = board[rule[0]]
+            let player2 = board[rule[1]]
+            let player3 = board[rule[2]]
+            
+            if player1 == player2, player2  == player3,!player1.isEmpty {
+                print("winner is \(player2)")
+                showAlert(msg: "WoW \(player3) you've won ")
+                return
+            }
+        }
+         
+        if !board.contains(""){
+            showAlert(msg: " the game is tied try again")
+        
+    }
+    }
+    func showAlert(msg: String) {
+        let alert = UIAlertController(title: "Winner", message: "Hurray! you have won", preferredStyle: .alert)
+        let action = UIAlertAction(title: "NewGame", style: .default) { _ in self.reset()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+        
+        func reset() {
+            board.removeAll()
+            loadBoard()
+            
+            for button in buttons {
+                button.setTitle(nil, for: .normal)
+            }
+            
+        }
+    
+    
+    func loadBoard(){
+        for i in 0..<buttons.count{
+        board.append("")
+    }
+}
 
 }
 
